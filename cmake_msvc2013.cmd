a0b011b04fa6aa7a2e2b0830226a83e07198a20a
Mark E Sowden <mark-sowden@outlook.com>
2021-03-17 21:37:45 +0000

    Removed batch files for older versions of msvc. Also commented out detection of WinSDK - paths should automatically get set up if it's installed, just let it use whatever the most appropriate path is.

--- cmake_msvc2013.cmd
+++
@@ -1,4 +0,0 @@
-mkdir build\msvc2013-x86
-pushd build\msvc2013-x86
-cmake -G "Visual Studio 12 2013" %* ../..
-popd
--- cmake_msvc2015.cmd
+++
@@ -1,4 +0,0 @@
-mkdir build\msvc2015-x86
-pushd build\msvc2015-x86
-cmake -G "Visual Studio 14 2015" %* ../..
-popd
