58a6b10b2603f4453912de96a57f0100448c4390
Michael Bullington <michael@bullington.xyz>
2025-06-02 19:55:44 -0400

    Use Ninja for building CMake

--- cmake_linux.sh
+++ cmake_linux.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 BUILD_DIR=build/gcc/debug
-DIRECTORY="gcc"
+DIRECTORY="clang-libc++"
 BUILDTYPE="debug"
 CMAKE_BUILD_TYPE="Debug"
-CMAKE_GENERATOR="Sublime Text 2 - Unix Makefiles"
+CMAKE_GENERATOR="Ninja"
 
 if [ "$#" -ne "2" ]; then
   echo "Usage cmake_linux.sh <gcc|clang|clang-libc++> <debug|release|reldeb>"
   exit 1
-fi  
+fi
 
-if [ "$#" -ge "1" ]; then 
+if [ "$#" -ge "1" ]; then
 
   if [ "$1" = "gcc" ]; then
     export CXX="g++"
@@ -21,20 +21,20 @@ if [ "$#" -ge "1" ]; then
 
   if [ "$1" = "clang" ]; then
     export CXX="clang++"
-    export CC="clang"   
+    export CC="clang"
     DIRECTORY="clang"
     CMAKE_CXX_FLAGS="-stdlib=libstdc++"
-  fi 
+  fi
 
   if [ "$1" = "clang-libc++" ]; then
     export CXX="clang++"
-    export CC="clang"   
+    export CC="clang"
     DIRECTORY="clang-libc++"
     CMAKE_CXX_FLAGS="-stdlib=libc++"
-  fi  
+  fi
 fi
 
-if [ "$#" -ge "2" ]; then 
+if [ "$#" -ge "2" ]; then
 
   if [ "$2" = "debug" ]; then
     BUILDTYPE="debug"
@@ -44,12 +44,12 @@ if [ "$#" -ge "2" ]; then
   if [ "$2" = "release" ]; then
     BUILDTYPE="release"
     CMAKE_BUILD_TYPE="Release"
-  fi  
+  fi
 
   if [ "$2" = "reldeb" ]; then
     BUILDTYPE="relwithdebinfo"
     CMAKE_BUILD_TYPE="relwithdebinfo"
-  fi    
+  fi
 fi
 
 BUILDDIR=build/$DIRECTORY/$BUILDTYPE
